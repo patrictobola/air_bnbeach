aa93e3c6a072a425c83da32f44447db07b35ceb9
patrictobola <tobola92@gmail.com>
2024-03-06 14:32:11 +0100

    done review cms

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserSeeder::class, ListingSeeder::class, PhotoSeeder::class, BookingSeeder::class]);
+        $this->call([UserSeeder::class, ListingSeeder::class, PhotoSeeder::class, BookingSeeder::class, ReviewSeeder::class]);
     }
 }
